ef81fed765ccd9ee2710398dcb4ee39299861036
devnazir <nazirbachrululum@gmail.com>
2023-09-05 16:18:43 +0700

    add skills section

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -12,3 +12,64 @@ export const MENUS = [
     path: "/experience",
   },
 ] as const;
+
+export const SKILLS = [
+  {
+    name: "HTML",
+    src: "/icons/html.png",
+  },
+  {
+    name: "CSS",
+    src: "/icons/css.png",
+  },
+  {
+    name: "SASS/SCSS",
+    src: "/icons/sass.png",
+  },
+  {
+    name: "Tailwind CSS",
+    src: "/icons/tailwind.svg",
+  },
+  {
+    name: "Styled Components",
+    src: "/icons/styled-com.svg",
+  },
+  {
+    name: "Ant Design",
+    src: "/icons/ant-design.svg",
+  },
+  {
+    name: "JavaScript",
+    src: "/icons/js.png",
+  },
+  {
+    name: "Node.js",
+    src: "/icons/nodejs.svg",
+  },
+
+  {
+    name: "TypeScript",
+    src: "/icons/ts.svg",
+  },
+
+  {
+    name: "React JS",
+    src: "/icons/react.png",
+  },
+  {
+    name: "Next JS",
+    src: "/icons/next.svg",
+  },
+  {
+    name: "Astro",
+    src: "/icons/astro.png",
+  },
+  {
+    name: "Apollo GraphQL",
+    src: "/icons/apollo-graphql.svg",
+  },
+  {
+    name: "Jotai",
+    src: "/icons/jotai-mascot.png",
+  },
+] as const;
